af4e3a322b98eaa41a21020a9d8a160637a6816a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-08-23 01:09:08 -0400

    added error handling

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from utils import read_text_file
 
 def main():
-  content = read_text_file('debug/tdest.txt')
+  content = read_text_file('debug/test.txt')
   print(content)
 
 if __name__ == '__main__':
