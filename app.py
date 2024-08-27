0ab9adac05720113f5942fcb336c446edca0c67b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-08-26 22:30:05 -0400

    fixed name

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from utils import read_text_file, extract_keywords, read_pdf_file
 
 def main():
-  desc = read_text_file('debug/description.txt')
+  desc = read_text_file('debug/desc.txt')
   desc_keywords = extract_keywords(desc)
 
-  resume = read_pdf_file('debug/resume2.pdf')
-  resume_keywords = extract_keywords(resume)
-
   print(desc_keywords)
-  print(resume_keywords)
+
   
 if __name__ == '__main__':
   main()
