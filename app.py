e856a1edbf210c4911446c75f8167123d0864a2a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-08-26 22:24:22 -0400

    cleaned up main function

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from utils import read_text_file, extract_keywords, read_pdf_file, create_text_file
+from utils import read_text_file, extract_keywords, read_pdf_file
 
 def main():
-  # desc = read_text_file('debug/description.txt')
-  # desc_keywords = extract_keywords(desc)
-  # print(desc_keywords)
-  # resume = read_pdf_file('debug/resume2.pdf')
-  # resume_keywords = extract_keywords(resume)
-  create_text_file('debug/', 'resume2', 'this is a samplessssssssssss')
-  
+  desc = read_text_file('debug/description.txt')
+  desc_keywords = extract_keywords(desc)
 
+  resume = read_pdf_file('debug/resume2.pdf')
+  resume_keywords = extract_keywords(resume)
+
+  print(desc_keywords)
+  print(resume_keywords)
+  
 if __name__ == '__main__':
   main()
