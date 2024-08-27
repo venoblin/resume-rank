6b8f85317b5e8132a20a28e03d66141e34554739
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-08-26 22:32:17 -0400

    added resume

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ def main():
   desc = read_text_file('debug/desc.txt')
   desc_keywords = extract_keywords(desc)
 
-  print(desc_keywords)
+  resume = read_pdf_file('debug/resume1.pdf')
+  resume_keywords = extract_keywords(resume)
 
-  
+  print({'desc': desc_keywords, 'resume': resume_keywords})
+
+
 if __name__ == '__main__':
   main()
