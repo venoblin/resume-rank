e59ded1c68fa3ceb1a6e5397b7031a64c73907dd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-08 17:52:42 -0400

    implemented file class

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from core.file import File
+from utils import compare_keywords
 
 def main():
   job = File('debug/job.txt')
-  resume = File('debug/resume1.pdf')
+  resume = File('debug/resume.txt')
 
-  print(job.extract_keywords())
+  job_keywords = job.extract_keywords()
+  resume_keywords = resume.extract_keywords()
+
+  print(compare_keywords(job_keywords, resume_keywords))
 
 if __name__ == '__main__':
   main()
