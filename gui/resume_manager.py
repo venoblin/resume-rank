3fb9a47ca0ee909e64c5f1c30c9b3f4fbb0cb24e
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-21 16:30:29 -0400

    added containers dir

--- gui/resume_manager.py
+++ gui/resume_manager.py
@@ -1,9 +1,16 @@
 from kivy.app import App
-from gui.widgets.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from gui.containers.window import Window
+from gui.widgets.header_label import HeaderLabel
 
 class ResumeManager(App):
     def build(self):
         self.title = "Resume Manager"
         window = Window()
+
+        resumes = BoxLayout(orientation='vertical')
+        resumes.add_widget(HeaderLabel(text='Resumes'))
+
+        window.add_widget(resumes)
 
         return window
