5b2e4bd7de4079947c42f66ec6f08b9c363b0a9b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-21 15:52:03 -0400

    created header label file

---
+++ gui/widgets/header_label.py
@@ -0,0 +1,5 @@
+from kivy.uix.label import Label
+
+class HeaderLabel(Label):
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
--- gui/window.py
+++ gui/window.py
@@ -1,5 +1,9 @@
 from kivy.uix.boxlayout import BoxLayout
 
 class Window(BoxLayout):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(self, **kwargs)
+        self.orientation = 'vertical'
+
+        resume_layout = BoxLayout(orientation='vertical')
+        
