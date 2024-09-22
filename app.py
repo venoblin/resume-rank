de75db353f59426d1c1c60b2abecf9d8afcce47a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-22 18:29:48 -0400

    worked on file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from gui.resume_manager import ResumeManager
+# from gui.resume_manager import ResumeManager
+from core.files import Files
 
 def main():
-  app = ResumeManager()
-  app.run()
+  # app = ResumeManager()
+  # app.run()
+  Files('files/')
 
 if __name__ == '__main__':
   main()
