7dcda3421a0efa7f49fd1ae5f609d3d5fd4ae88c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-22 16:11:50 -0400

    modified setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from utils import run_command
 
 def main():
   run_command('mkdir files')
+  run_command('mkdir files/resumes')
   run_command('python -m textblob.download_corpora')
   nltk.download('punkt_tab')
 
