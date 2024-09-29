dfd25875433e98adaede42242c5ee7ed4b3ee705
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-09-29 14:00:48 -0400

    created main windows

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication
+from gui.windows.main_window import MainWindow
 import sys
 
 def main():
   app = QApplication(sys.argv)
 
-  window = QWidget()
+  window = MainWindow()
   window.show()
 
   app.exec()
---
+++ gui/windows/main_window.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QMainWindow
+
+class MainWindow(QMainWindow):
+  def __init__(self):
+    super(MainWindow, self).__init__()
+
+    self.setWindowTitle('Resume Manager')
+  
