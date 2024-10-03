8f908d01e9a42f88b9142f4b6b59b38690d897e9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-02 22:35:53 -0400

    created button widget and added one to resumes layout

--- gui/widgets/button.py
+++ gui/widgets/button.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QPushButton
 
 class Button(QPushButton):
-  def __init__(self):
-    super(Button, self).__init__()
+  def __init__(self, text=''):
+    super(Button, self).__init__(text)
--- gui/widgets/containers/resumes.py
+++ gui/widgets/containers/resumes.py
@@ -1,10 +1,13 @@
 from gui.widgets.container import Container
 from gui.widgets.header_label import HeaderLabel
+from gui.widgets.button import Button
 
 class Resumes(Container):
   def __init__(self):
     super(Resumes, self).__init__()
 
     header = HeaderLabel(text='Resumes')
+    upload_resume_btn = Button(text='Upload Resume')
 
     self.layout.addWidget(header)
+    self.layout.addWidget(upload_resume_btn)
