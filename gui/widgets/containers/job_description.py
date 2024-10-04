4a764fedb6f5dab75c2058565565ac8b58122870
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-03 22:58:49 -0400

    finished basic job desc container

--- gui/widgets/containers/job_description.py
+++ gui/widgets/containers/job_description.py
@@ -1,6 +1,13 @@
 from gui.widgets.container import Container
+from gui.widgets.header_label import HeaderLabel
+from gui.widgets.button import Button
 
 class JobDescription(Container):
   def __init__(self):
     super().__init__()
-    
+
+    header = HeaderLabel(text='Job Description')
+    find_btn = Button(text='Find Best Resume')
+
+    self.layout.addWidget(header)
+    self.layout.addWidget(find_btn)
