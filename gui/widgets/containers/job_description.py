32794f6c39a17617c2c61578b5c8e3f930c97900
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-05 14:18:58 -0400

    added textarea to resumes

--- gui/widgets/containers/job_description.py
+++ gui/widgets/containers/job_description.py
@@ -1,3 +1,4 @@
+from PySide6.QtWidgets import QPlainTextEdit
 from gui.widgets.container import Container
 from gui.widgets.header_label import HeaderLabel
 from gui.widgets.button import Button
@@ -7,7 +8,10 @@ class JobDescription(Container):
     super().__init__()
 
     header = HeaderLabel(text='Job Description')
+    textarea = QPlainTextEdit()
+    textarea.setPlaceholderText('Past job description here...')
     find_btn = Button(text='Find Best Resume')
 
     self.layout.addWidget(header)
+    self.layout.addWidget(textarea)
     self.layout.addWidget(find_btn)
