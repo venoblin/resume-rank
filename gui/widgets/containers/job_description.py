b79a4f1fce67dd7589ec6065ddee0519e843dfd8
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-17 23:37:29 -0400

    made textarea global

--- gui/widgets/containers/job_description.py
+++ gui/widgets/containers/job_description.py
@@ -4,17 +4,19 @@ from gui.widgets.header_label import HeaderLabel
 from gui.widgets.button import Button
 
 class JobDescription(Container):
+  textarea: QPlainTextEdit
+  
   def __init__(self):
     super().__init__()
 
     header = HeaderLabel(text='Job Description')
-    textarea = QPlainTextEdit()
-    textarea.setPlaceholderText('Past job description here...')
+    self.textarea = QPlainTextEdit()
+    self.textarea.setPlaceholderText('Past job description here...')
     find_btn = Button(text='Find Best Resume')
     find_btn.clicked.connect(self._find_resume_handler)
 
     self.layout.addWidget(header)
-    self.layout.addWidget(textarea)
+    self.layout.addWidget(self.textarea)
     self.layout.addWidget(find_btn)
 
   def _find_resume_handler(self):
