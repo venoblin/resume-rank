00fa2725d910e31f87b44417ac080efe1eb887a8
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-17 23:45:46 -0400

    fixed typo

--- gui/widgets/containers/job_description.py
+++ gui/widgets/containers/job_description.py
@@ -11,7 +11,7 @@ class JobDescription(Container):
 
     header = HeaderLabel(text='Job Description')
     self.textarea = QPlainTextEdit()
-    self.textarea.setPlaceholderText('Past job description here...')
+    self.textarea.setPlaceholderText('Paste job description here...')
     find_btn = Button(text='Find Best Resume')
     find_btn.clicked.connect(self._find_resume_handler)
 
@@ -20,4 +20,6 @@ class JobDescription(Container):
     self.layout.addWidget(find_btn)
 
   def _find_resume_handler(self):
-    print('resume')
+    content = self.textarea.toPlainText()
+
+    print(content)
