8d799c2425d0928bd57679a29056cb46a1cf2769
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-10-19 01:52:30 -0400

    worked on dir

--- core/directory.py
+++ core/directory.py
@@ -11,12 +11,12 @@ class Directory(File):
     files = []
 
     for file_name in os.listdir(self.src):
-        file_path = os.path.join(self.src, file_name)
+      file_path = os.path.join(self.src, file_name)
 
-        if os.path.isfile(file_path):
-            files.append({
-              'file_path': file_path,
-              'file_name': file_name
-            })
+      if os.path.isfile(file_path):
+        files.append({
+          'file_path': file_path,
+          'file_name': file_name
+        })
 
     return files
